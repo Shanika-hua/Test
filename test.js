d40352498954899bd379a996bc20eed4ea58eba5
郭莉华 <lihuag@keyayun.com>
2020-06-30 19:57:01 +0800

    'sourceTree  first test'

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-const Koa = require('koa');
-const fs = require('fs');
+const Koa = require("koa");
+const fs = require("fs");
 const app = new Koa();
 
-const main = async function(ctx){
-    ctx.response.type = 'html';
-    ctx.response.body = await fs.readFile('./demo.html', 'utf8');
-
-}
+const main = async function (ctx) {
+  ctx.response.type = "html";
+  ctx.response.body = await fs.readFile("./demo.html", "utf8");
+  console.log("xxxx");
+};
 
 app.use(main);
 app.listen(3002);
-
