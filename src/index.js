112f7758ffc16d5855615138363a9e3a6cc8e9ab
郭莉华 <lihuag@keyayun.com>
2020-06-30 21:17:13 +0800

    'three'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
+console.log("yyy");
 console.log("zzz");
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
